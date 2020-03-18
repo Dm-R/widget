ad9e4de9a968c441aa3ce73ccdbac44fafb31228
Dmitry <rikovdmitri39@gmail.com>
2020-03-18 23:19:42 +0300

    linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,58 @@
+/* eslint-disable linebreak-style */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'css-hot-loader',
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === 'development',
-                            reloadAll: true,
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader',
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',
-        }),
-    ],
-    devServer: {
-        before(app, server) {
-            server._watch('src/index.html');
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-        contentBase: './dist',
-        port: 1111,
-        hot: true,
-    }
-}
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'css-hot-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  devServer: {
+    before(app, server) {
+      // eslint-disable-next-line no-underscore-dangle
+      server._watch('src/index.html');
+    },
+    contentBase: './dist',
+    port: 1111,
+    hot: true,
+  },
+};
