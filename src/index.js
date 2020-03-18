4dfe50d2539efe6ffeaf5c5c283fd7de4fd218ff
Dmitry <rikovdmitri39@gmail.com>
2020-03-18 23:17:03 +0300

    linting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable linebreak-style */
 import './scss/main.scss';
 
-// import {
-//   SEND_REQUEST,
-//   addRequestSuccess, addRequestError 
-// } from './actions';
+import {
+  SEND_REQUEST,
+  addRequestSuccess, addRequestError,
+} from './actions';
 
 const redux = require('redux');
 const thunkMiddleware = require('redux-thunk').default;
@@ -12,38 +15,32 @@ const thunkMiddleware = require('redux-thunk').default;
 const { applyMiddleware } = redux;
 const { createStore } = redux;
 
-
-const State = {
-  count: 0,
-};
-const fetchUsers = (url) => {
-  return async (dispatch) => {
-    dispatch(SEND_REQUEST);
-    try {
-      let users = await fetch(url);
-      users = await users.json();
-      const usersUrl = [];
-      // eslint-disable-next-line no-restricted-syntax
-      for (const user of users) {
-        usersUrl.push(user.url);
-      }
-      const dividedBy10UsersUrl = [];
-      const countParts = Math.floor(usersUrl.length/10);
-      for (let i = 0, j = 1; i <= countParts; i += 1, j += 1) {
-        dividedBy10UsersUrl = [...dividedBy10UsersUrl, usersUrl.slice(i * 10, j * 10)]
-      }
-      const usersWithFullInfo = [];
-      // eslint-disable-next-line no-restricted-syntax
-      for (const each10Url of dividedUsersUrl) {
-        const users = await Promise.all([...(each10Url.map((url) => fetch(url)))]);
-        usersWithFullInfo = [...usersWithFullInfo, users];
-      }
-      dispatch(addRequestSuccess(usersWithFullInfo));
-    } catch (error) {
-      dispatch(addRequestError(error));
+const fetchUsers = (url) => async (dispatch) => {
+  dispatch(SEND_REQUEST);
+  try {
+    let users = await fetch(url);
+    users = await users.json();
+    const usersUrl = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const user of users) {
+      usersUrl.push(user.url);
     }
-
-  };
+    let dividedBy10UsersUrl = [];
+    const countParts = Math.floor(usersUrl.length / 10);
+    for (let i = 0, j = 1; i <= countParts; i += 1, j += 1) {
+      dividedBy10UsersUrl = [...dividedBy10UsersUrl, usersUrl.slice(i * 10, j * 10)];
+    }
+    let usersWithFullInfo = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const each10Url of dividedBy10UsersUrl) {
+      // eslint-disable-next-line max-len
+      const next10users = await Promise.all([...(each10Url.map((singleUrl) => fetch(singleUrl)))]);
+      usersWithFullInfo = [...usersWithFullInfo, next10users];
+    }
+    dispatch(addRequestSuccess(usersWithFullInfo));
+  } catch (error) {
+    dispatch(addRequestError(error));
+  }
 };
 const reduser = (state, action) => {
   switch (action.type) {
