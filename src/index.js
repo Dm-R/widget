4ee563aa479a4f9044dd5a3e515d3734abc2acfa
Dmitry <rikovdmitri39@gmail.com>
2020-03-18 22:47:49 +0300

    add index.html main.scss reducers.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import {
-  SEND_REQUEST,
-  addRequestSuccess, addRequestError 
-} from './actions';
+import './scss/main.scss';
+
+// import {
+//   SEND_REQUEST,
+//   addRequestSuccess, addRequestError 
+// } from './actions';
 
 const redux = require('redux');
 const thunkMiddleware = require('redux-thunk').default;
@@ -17,23 +19,30 @@ const State = {
 const fetchUsers = (url) => {
   return async (dispatch) => {
     dispatch(SEND_REQUEST);
-    let users = await fetch(url);
-    users = await users.json();
-    const usersUrl = [];
-    // eslint-disable-next-line no-restricted-syntax
-    for (const user of users) {
-      usersUrl.push(user.url);
+    try {
+      let users = await fetch(url);
+      users = await users.json();
+      const usersUrl = [];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const user of users) {
+        usersUrl.push(user.url);
+      }
+      const dividedBy10UsersUrl = [];
+      const countParts = Math.floor(usersUrl.length/10);
+      for (let i = 0, j = 1; i <= countParts; i += 1, j += 1) {
+        dividedBy10UsersUrl = [...dividedBy10UsersUrl, usersUrl.slice(i * 10, j * 10)]
+      }
+      const usersWithFullInfo = [];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const each10Url of dividedUsersUrl) {
+        const users = await Promise.all([...(each10Url.map((url) => fetch(url)))]);
+        usersWithFullInfo = [...usersWithFullInfo, users];
+      }
+      dispatch(addRequestSuccess(usersWithFullInfo));
+    } catch (error) {
+      dispatch(addRequestError(error));
     }
-    const dividedBy10UsersUrl = [];
-    const countParts = Math.floor(usersUrl.length/10);
-    for (let i = 0, j = 1; i <= countParts; i += 1, j += 1) {
-      dividedUsersUrl = [...dividedUsersUrl, usersUrl.slice(i * 10, j * 10)]
-    }
-    const usersWithFullInfo = [];
-    // eslint-disable-next-line no-restricted-syntax
-    for (const each10Url of dividedUsersUrl) {
-      const users = await Promise.all([...each10Url]);
-    }
+
   };
 };
 const reduser = (state, action) => {
